986c999e9bda59e781531bcd107992e501fba5aa
skywss27 <skywss27@outlook.com>
2018-09-29 16:37:02 +0800

    commit node

---
+++ WorkFlow/Models/Entities/Line.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WorkFlow.Models.Entities
+{
+    public class Line
+    {
+        public string Key { set; get; }
+        public string Type { set; get; }
+        // node key 
+        public string From { set; get; }
+        public string To { set; get; }
+        public string Name { set; get; }
+
+    }
+}
---
+++ WorkFlow/Models/Entities/Node.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WorkFlow.Models.Entities
+{
+    public class Node
+    {
+        //node key
+        public string Key { set; get; }
+        public string Name { set; get; }
+        // node location
+        public int Left { set; get; }
+        public int Top { set; get; }
+        public string Type { set; get; }
+        // node size
+        public int Width { set; get; }
+        public int Height { set; get; }
+        public bool Alt { set; get; }
+    }
+
+}
